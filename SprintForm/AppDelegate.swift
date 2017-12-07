c9fb9d73f6d382ba75bde3e28e3e1d2dd7285a15
chris <chris@chriss-MBP.fios-router.home>
2017-12-07 14:41:15 -0500

    Updates to Alamofire.  Typo fix in AppDelegate.

--- SprintForm/AppDelegate.swift
+++ SprintForm/AppDelegate.swift
@@ -89,7 +89,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         "7455",
         "7968",
         "anna"
-    ],
+    ];
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
